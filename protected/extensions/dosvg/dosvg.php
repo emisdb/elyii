a3789a1553f507ea6b095d0f7a3109c5ac501fe5
Denis <dentsi@yahoo.com>
2015-09-26 18:11:31 +0300

    functions in svg

--- protected/extensions/dosvg/dosvg.php
+++ protected/extensions/dosvg/dosvg.php
@@ -26,48 +26,71 @@ class dosvg extends CWidget {
     public function init()
 	{
     }
+    public function stylejs(&$res)
+	{
+ 			$res.="<style>\n";
+			$res.=".road-line{stroke:#336666; stroke-width:4;fill:none;}\n";
+			$res.="line:hover, polyline:hover{stroke:#f72579;stroke-width:5;opacity:.9;}\n";
+			$res.="circle:hover{fill:#f72579;stroke-width:3;stroke:#f72579;}\n";
+			$res.=".city-title{font-size:8pt;font-weight:normal;font-family:Arial Black;}\n";
+			$res.=".road-title{fill:#f72579;font-size:8pt;font-weight:normal;font-family:Arial Black;}\n";
+			$res.="</style>\n";
+			$res.='<script type="text/ecmascript"> <![CDATA[';
+			$res.="function showtitle(evt,id){ \n";
+			$res.='evt.target.parentNode.getElementById("textroad"+id).setAttribute("fill-opacity",.9);';
+			$res.="}\n";
+			$res.="function hidetitle(evt,id){ \n";
+			$res.='evt.target.parentNode.getElementById("textroad"+id).setAttribute("fill-opacity",0);';
+			$res.="}\n";
+			$res.="]]> </script>\n";
+	
+
+
+   }
    
    private function fetchRow($_par,$_next) 
   {
   }
 	private function runMap()
 	{
-			$res="<style>\n";
-			$res.="line:hover, polyline:hover{stroke:#f72579;stroke-width:5;opacity:.9;}\n";
-			$res.="circle:hover{fill:#f72579;stroke-width:3;stroke:#f72579;}\n";
-			$res.=".city-title{font-size:8pt;font-weight:normal;font-family:Arial Black;}\n";
-			$res.=".road-title{fill:#f72579;font-size:8pt;font-weight:normal;font-family:Arial Black;}\n";
-			$res.="</style>\n";
-			$ii=0;
-	          foreach ($this->tree as $value) {
-				if($value['position']->type=='c'){
-					$res.='<circle id="city'.$value['id']
-						.'" cx="'.$value['position']->coors[0].'" cy="'.$value['position']->coors[1].'" r="'.$value['position']->coors[2].'" fill="#dededd" stroke="#336666" stroke-width="1" /> ';			
-					$res.='<text class="city-title" x="'.$value['position']->title[0].'" y="'.$value['position']->title[1]
-							.'" fill="#000">'.$value['name'];			
-					$res.='<set attributeName="fill" to="#f72579" begin="city'.$value['id'].'.mouseover" end="city'.$value['id'].'.mouseout" ></text> ';			
-				  }
-				else {
-					if($value['position']->type=='p'){
-						  $res.='<polyline id="road'.$value['id'].'" stroke="#336666" stroke-width="4" fill="none" points="'.$value['position']->coors.'" />';	
-					}
-					  elseif($value['position']->type=="l")
-					  {
-						  $res.='<line id="road'.$value['id'].'" stroke="#336666" stroke-width="4"'
-								  .' x1="'.$value['position']->coors[0].'"'
-								  .' y1="'.$value['position']->coors[1].'"'
-								  .' x2="'.$value['position']->coors[2].'"'
-								  .' y2="'.$value['position']->coors[3].'"'
-								  .'/>';			
-					  }
-					  $res.='<text class="road-title" fill-opacity="0" x="'.$value['position']->title[0].'" y="'.$value['position']->title[1]
-							  .'" fill="#000">'.$value['name'];			
-					  $res.='<set attributeName="fill-opacity" to=".9" begin="road'.$value['id'].'.mouseover" end="road'.$value['id'].'.mouseout" ></text> ';			
-				  }
+		$res="";
+		$this->stylejs($res);
+		$ii=0;
+		foreach ($this->tree as $value) {
+		  if($value['position']->type=='c'){
+			  $res.='<circle id="city'.$value['id']
+				  .'" cx="'.$value['position']->coors[0].'" cy="'.$value['position']->coors[1].'" r="'.$value['position']->coors[2].'" fill="#dededd" stroke="#336666" stroke-width="1" /> ';			
+			  $res.='<text class="city-title" x="'.$value['position']->title[0].'" y="'.$value['position']->title[1]
+					  .'" fill="#000">'.$value['name'];			
+			  $res.='<set attributeName="fill" to="#f72579" begin="city'.$value['id'].'.mouseover" end="city'.$value['id'].'.mouseout" >';
+				$res.='</text> ';			
+			}
+		  else {
+			  if($value['position']->type=='p'){
+//					$res.='<polyline id="road'.$value['id'].'" stroke="#336666" stroke-width="4" fill="none" points="'.$value['position']->coors.'" />';	
+					$res.='<polyline class="road-line" id="road'.$value['id'].'" points="'.$value['position']->coors.'"'
+							. ' onmouseover="showtitle(evt,'.$value['id'].')"'	
+							. ' onmouseout="hidetitle(evt,'.$value['id'].')" />';	
+			  }
+				elseif($value['position']->type=="l")
+				{
+					$res.='<line id="road'.$value['id'].'" stroke="#336666" stroke-width="4"'
+							.' x1="'.$value['position']->coors[0].'"'
+							.' y1="'.$value['position']->coors[1].'"'
+							.' x2="'.$value['position']->coors[2].'"'
+							.' y2="'.$value['position']->coors[3].'"'
+							. ' onmouseover="showtitle(evt,'.$value['id'].')"'	
+							. ' onmouseout="hidetitle(evt,'.$value['id'].')" />';	
+				}
+				$res.='<text class="road-title" id="textroad'.$value['id'].'" fill-opacity="0" x="'.$value['position']->title[0].'" y="'.$value['position']->title[1]
+						.'" fill="#000">'.$value['name'];			
+//				$res.='<set attributeName="fill-opacity" to=".9" begin="road'.$value['id'].'.mouseover" end="road'.$value['id'].'.mouseout" >';			
+				$res.='</text> ';			
+			}
 /**/				  $ii++;
-				  $res.="/n";
-            }
-			echo $res;
+			$res.="\n";
+	  }
+		echo $res;
 		
 	}
 	public function run() 
